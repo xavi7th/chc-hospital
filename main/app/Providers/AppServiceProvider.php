7b931e467c2fb26ff7d0e8c50491e1e2161f5128
leinad <xavi7th@gmail.com>
2020-06-24 19:47:48 +0100

    Fix: No gd library on upstream library.  Switch to imagick for intervention image processing

--- main/app/Providers/AppServiceProvider.php
+++ main/app/Providers/AppServiceProvider.php
@@ -7,30 +7,30 @@ use Nwidart\Modules\Facades\Module;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
-	/**
-	 * Register any application services.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		//
-
-	}
-
+  /**
+   * Register any application services.
+   *
+   * @return void
+   */
+  public function register()
+  {
+    Schema::defaultStringLength(191);
+  }
 
 
-	/**
-	 * Bootstrap any application services.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		//
 
-	}
+  /**
+   * Bootstrap any application services.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+    //
+
+  }
 }
